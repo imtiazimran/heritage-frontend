ead6174b67418363584ffdea3d67be53c1e1bf5f
imtiazimran <imtiazimran@outlook.com>
2024-08-18 04:45:08 +0600

    feature updated

--- src/pages/home/Feature.tsx
+++ src/pages/home/Feature.tsx
@@ -7,7 +7,7 @@ const Feature = () => {
   return (
     <Container className="py-8 my-10  ">
       {/* First Section */}
-      <div className="flex flex-wrap justify-between items-center  mb-10 h-screen">
+      <div className="flex flex-wrap justify-between gap-32 md:gap-0 items-center mb-10 h-screen my-28 md:my-0">
         {/* Background Image */}
         <div className="w-full md:w-1/2 h-64 relative mb-8 md:mb-0 my-10">
           <img
@@ -46,8 +46,23 @@ const Feature = () => {
       </div>
 
       {/* Second Section */}
-      <div className="flex flex-wrap justify-between items-center mb-10 h-screen">
-        <div className="w-full md:w-1/2 pl-8">
+      <div className="flex flex-wrap flex-row-reverse justify-between gap-20 md:gap-0 items-center mb-10 h-screen mt-56 my-28 md:my-0 md:mt-0">
+        <div className="w-full md:w-1/2 h-64 relative mb-8 md:mb-0">
+          <img
+            src="https://bandaproperty.com/wp-content/uploads/2023/03/Perry-Street_Living-Room_040-scaled.jpg"
+            alt="Background Image"
+            className="w-[70%] absolute top-2/4 left-0 h-full object-cover rounded-lg"
+          />
+          {/* Overlay Image */}
+
+          <FaPlayCircle className=" z-50 text-primary absolute left-[20%] text-5xl top-[35%]" />
+          <img
+            src="https://bandaproperty.com/wp-content/uploads/2023/06/230518-Banda-Paris-02-Print-scaled.jpg"
+            alt="Overlay Image"
+            className="absolute -top-1/5 left-1/4 w-[70%] h-auto rounded-lg border-4 border-white shadow-lg z-10"
+          />
+        </div>
+        <div className="w-full md:w-1/2 pl-8 mt-11">
           <div className="flex items-center gap-2 mb-4">
             <div className="w-8 border h-[1px] border-secondary"></div>
             <p className="text-secondary font-semibold">Property Buying</p>
@@ -65,25 +80,10 @@ const Feature = () => {
             </Button>
           </Link>
         </div>
-        <div className="w-full md:w-1/2 h-64 relative mb-8 md:mb-0">
-          <img
-            src="https://bandaproperty.com/wp-content/uploads/2023/03/Perry-Street_Living-Room_040-scaled.jpg"
-            alt="Background Image"
-            className="w-[70%] absolute top-2/4 left-0 h-full object-cover rounded-lg"
-          />
-          {/* Overlay Image */}
-
-          <FaPlayCircle className=" z-50 text-primary absolute left-[20%] text-5xl top-[35%]" />
-          <img
-            src="https://bandaproperty.com/wp-content/uploads/2023/06/230518-Banda-Paris-02-Print-scaled.jpg"
-            alt="Overlay Image"
-            className="absolute -top-1/5 left-1/4 w-[70%] h-auto rounded-lg border-4 border-white shadow-lg z-10"
-          />
-        </div>
       </div>
 
       {/* Third Section */}
-      <div className="flex flex-wrap justify-between items-center h-screen">
+      <div className="flex flex-wrap justify-between flex-row md:flex-row gap-32 md:gap-0 items-center mb-10 h-screen mt-56 md:mt-0 my-28 md:my-0">
         {/* Background Image */}
         <div className="w-full md:w-1/2 h-64 relative mb-8 md:mb-0">
           <img
